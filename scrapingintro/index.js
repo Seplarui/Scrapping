ead0535766c016f1838c83b3c3bc89fcb3cf58cc
Sebas <seplarui@gmail.com>
2020-02-02 12:40:27 +0100

    index.js

--- scrapingintro/index.js
+++ scrapingintro/index.js
@@ -6,6 +6,9 @@ const cheerio = require('cheerio')
 async function main() {
     const html = await request.get("https://reactnativetutorial.net/css-selectors/")
     fs.writeFileSync("./test.html", html)
+    const $= await cheerio.load(html);
+    const theText = $("h1").text();
+    console.log(theText);
 }
 
 main();
